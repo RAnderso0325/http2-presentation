e0ec96855125a080f5dfa54b7958a31e0ec859c8
James M Snell <jasnell@gmail.com>
2018-02-07 16:16:35 -0800

    Tweaks

--- server.js
+++ server.js
@@ -12,11 +12,42 @@ const server = http2.createSecureServer({ key, cert })
 server.on('stream', (stream, headers) => {
   let p = headers[':path']
   if (p === '/' || p === '/index') {
-    p = '/index.html'
+    p = '/index.html';
+
+    [
+      'css/reveal.css',
+      'css/theme/nodejs.css',
+      'lib/css/zenburn.css',
+      'lib/font/source-sans-pro/source-sans-pro.css',
+      'css/print/paper.css'
+    ].forEach((i) => {
+      stream.pushStream({ ':path': `/${i}` }, (err, stream) => {
+        pino.info({ 'method': 'push', ':path': `/${i}`, 'root': localPath }, '200')
+        stream.respondWithFile(`./${i}`, { 'content-type': 'text/css' });
+      });
+    });
+
+    [
+      'js/reveal.js',
+      'lib/js/head.min.js',
+      'plugin/markdown/marked.js',
+      'plugin/markdown/markdown.js',
+      'plugin/highlight/highlight.js',
+      'plugin/zoom-js/zoom.js',
+      'plugin/notes/notes.js'
+    ].forEach((i) => {
+      stream.pushStream({ ':path': `/${i}` }, (err, stream) => {
+        pino.info({ 'method': 'push', ':path': `/${i}`, 'root': localPath }, '200')
+        stream.respondWithFile(`./${i}`, {
+          'content-type': 'application/javascript'
+        });
+      });
+    });
 
     fs.readdir('./media', (err, items) => {
       items.forEach((i) => {
         const file = `/media/${i}`
+        pino.info({ 'method': 'push', ':path': file, 'root': localPath }, '200')
         stream.pushStream({ ':path': file }, (err, stream) => {
           stream.respondWithFile(`.${file}`)
         })
@@ -27,12 +58,12 @@ server.on('stream', (stream, headers) => {
   const localPath = path.resolve(__dirname, `.${p}`)
   fs.stat(localPath, (err, stat) => {
     if (err) {
-      pino.error({ ':path': p, 'root': localPath }, '404')
+      pino.error({ 'method': 'get', ':path': p, 'root': localPath }, '404')
       stream.respond({ ':status': 404 })
       stream.end('Not Found')
       return
     }
-    pino.info({ ':path': p, 'root': localPath }, '200')
+    pino.info({ 'method': 'get', ':path': p, 'root': localPath }, '200')
     stream.respond()
     const readStream = fs.createReadStream(localPath)
     readStream.pipe(stream)
